53a48f5637022d2e4df15dbc1f75192e95f1dc26
Nikunj <nikunjkhandekwal1234@gmail.com>
2022-07-28 16:30:02 +0530

    Completed 5th program of the Striver's Recursion Series

--- src/Recursion/Ques5.java
+++ src/Recursion/Ques5.java
@@ -1,4 +1,22 @@
 package Recursion;
 
+import java.util.Scanner;
+
+/**
+ *  Write a Program to print from N to 1 using backtracking method !!
+ */
 public class Ques5 {
+    public static void BacktrackingLinearPrint(int i , int n ){
+        if( i < 1)
+            return ;
+        BacktrackingLinearPrint(i-1,n+1);
+        System.out.println(n);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number you want to print from that number to 1 !!");
+        int n = scanner.nextInt();
+        BacktrackingLinearPrint(n,1);
+    }
 }
