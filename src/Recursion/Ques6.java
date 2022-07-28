b9fcc8e93f42009c01f48901b67c1b5938706602
Nikunj <nikunjkhandekwal1234@gmail.com>
2022-07-28 16:06:24 +0530

    Completed 6th program of the Striver's Recursion Series

---
+++ src/Recursion/Ques6.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+import java.util.Scanner;
+
+/**
+ * Write a program to print the sum of first N numbers
+ */
+
+public class Ques6 {
+    public static void SumOfFirstNNumbers(int i , int sum ){
+        if(i<1){
+            System.out.println(sum);
+            return;
+        }
+        SumOfFirstNNumbers(i-1,sum+i);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number till which you have to find the sum ");
+        int n = scanner.nextInt();
+        SumOfFirstNNumbers(n,0);
+    }
+}
